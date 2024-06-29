ad22e079d574236a91c4dbf06fcb0c356e656513
Mehedi-Hassan-Patwary <md.mehedi.hassan.patwary.cse@gmail.com>
2024-06-28 21:44:25 -0700

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+import "./App.css";
+
 
 function App() {
 
 
-  return <div>place holder</div>
+  return (
+      <center class='todo-container'> 
+
+          <AppName/>
+          <AddTodo></AddTodo>
+
+          <div className ="items-container">
+          <TodoItem1 />
+          <TodoItem2 />
+          </div>
+
+   </center>
+  )
 }
 
 export default App;
